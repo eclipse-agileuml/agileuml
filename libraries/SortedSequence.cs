44da348f3a4b250555d958bc8167b8380e68b546
Zishan Rahman <zishanfrahman@outlook.com>
2025-04-23 18:49:34 +0100

    Rewrite SortedSequence C# class template to remove main method and implement all constructors and class attributes

--- libraries/SortedSequence.cs
+++ libraries/SortedSequence.cs
@@ -1,7 +1,15 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
-class SortedSequence {
-    static void Main() {
-        Console.Writeline("Hello World");
+class SortedSequence: List<T> {
+    ArrayList elements = new ArrayList();
+    
+    public SortedSequence() {}
+
+    public SortedSequence(List<T> col) {
+        elements = new ArrayList();
+        elements.AddRange(col);
+        elements.Sort();
     }
 }
