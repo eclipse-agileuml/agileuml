bbb349d90249740998ab4c9258bcf026335fa1b3
Zishan Rahman <zishanfrahman@outlook.com>
2025-07-11 23:23:26 +0100

    Add subRows, subMatrix and matrixExcludingRowColumn methods to C++ MatrixLib implementation

--- libraries/MatrixLib.cpp
+++ libraries/MatrixLib.cpp
@@ -6,6 +6,7 @@ using std::vector;
 using std::cout;
 using std::endl;
 
+template <typename T>
 class MatrixLib {
     public:
         MatrixLib() {};
@@ -22,15 +23,48 @@ class MatrixLib {
         };
         static vector<vector<double>> rowMultiplication(vector<vector<double>> m1, vector<vector<double>> m2) {
             vector<vector<double>> result;
-            for (vector<double> row : m1) {
+            for (vector<double> row: m1) {
                 result.push_back(rowMult(row, m2));
             }
             return result;
         };
+        static vector<T> subRows(vector<T> m, vector<int> s) {
+            vector<T> result;
+            for (auto integer :s) {
+                if ((integer >= 0) && (integer <= m.size() - 1)) {
+                    result.push_back(m[integer]);
+                }
+            }
+            return result;
+        }
+        static vector<T> subMatrix(vector<vector<T>> m, vector<int> rows, vector<int> cols) {
+            vector<T> result;
+            for (auto row: rows) {
+                if ((row >= 0) && (row <= m.size() - 1)) {
+                    result.push_back(subRows(m[row], cols));
+                }
+            }
+            return result;
+        }
+        static vector<vector<T>> matrixExcludingRowColumn(vector<T> m, int row, int col) {
+            vector<vector<T>> result;
+            for (int i = 0; i < m.size(); i++) {
+                if (i == row) {
+                    vector<T> r = m[i];
+                    for (int j = 0; j < r.size(); i++) {
+                        if (j != col) {
+                            vector<T> subrow = r[j];
+                            result.push_back(subrow);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
 };
 
 int main() {
-    vector<vector<double>> list = MatrixLib::rowMultiplication({{1.0, 2.0}, {3.0, 4.0}}, {{5.0, 6.0}, {7.0, 8.0}});
+    vector<vector<double>> list = MatrixLib<void>::rowMultiplication({{1.0, 2.0}, {3.0, 4.0}}, {{5.0, 6.0}, {7.0, 8.0}});
     // cout << list.size() << endl;
     for (auto value : list) {
         for (auto innervalue : value) {
