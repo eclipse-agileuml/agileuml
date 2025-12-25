39bf3b4116c0e3e89209a5e69f1536bf599d0ae1
Zishan Rahman <zishanfrahman@outlook.com>
2025-12-25 12:11:28 +0000

    Implement matrixSubtraction in C++ MatrixLib

--- libraries/MatrixLib.cpp
+++ libraries/MatrixLib.cpp
@@ -541,6 +541,17 @@ class MatrixLib {
             }
             return res;
         }
+        static vector<T> matrixSubtraction(vector<T> m1, vector<T> m2) {
+            vector<T> res = {};
+
+            for (int i = 0; i < m1.size(); i++) {
+                vector<T> sq1 = (vector<T>) m1[i];
+                vector<T> sq2 = (vector<T>) m2[i];
+                res.push_back(rowSubtraction(sq1, sq2));
+            }
+
+            return res;
+        }
 };
 
 int main() {
